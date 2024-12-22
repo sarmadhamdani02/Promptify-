eab64d5ee890e3fe37c6c9e38d5ef7a9c28a040b
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-12-23 00:23:04 +0500

    update dbConnect.ts

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import { promises } from "dns";
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("dbConnect.ts : dbConnect() : Already Connected to DB ✔ ");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+
+    connection.isConnected = db.connections[0].readyState;
+    console.error("dbConnect.ts, : dbConnect() :: DB Connected Successfully ✔");
+  } catch (error) {
+    console.error(
+      "dbConnect.ts",
+      " :: dbConnect() :: DB Connection Error ❌ : ",
+      error
+    );
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
