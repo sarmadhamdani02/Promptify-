4457d047695e296fc0ce659d250408423bd75fed
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2025-02-08 00:35:49 +0500

    update PromptifyLogo.tsx

--- src/components/PromptifyLogo.tsx
+++ src/components/PromptifyLogo.tsx
@@ -5,11 +5,12 @@ const PromptifyLogo = ({
     className = '',
     textClassName = ' text-gray-800 ',
     zapClassName = 'text-blue-500 ',
+    zapSize = '32'
 }) => {
     return (
         <a href='' className={`flex items-center justify-center gap-0 text-4xl font-bold text-gray-900 ${className}`}>
             <span className={` ${textClassName}`}>P</span>
-            <Zap className={`${zapClassName}`} size={32} />
+            <Zap className={`${zapClassName}`} size={{zapSize}} />
             <span className={` ${textClassName}`}>omptify</span>
         </a>
     )
